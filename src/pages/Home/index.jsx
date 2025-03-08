770d93ccf2f6d7ff5977b176223848b8ea6ac58a
Amin Houssni <houssni.amin@outlook.fr>
2025-03-08 10:28:24 +0100

    Created titleContainer section in the Home page

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import "./home.css"
+
+export default function Home() {
+  return (
+    <div className="homeContainer">
+      <section className="titleContainer">
+        <div className="shadowTitle">
+          <h1>42 : APPRENDRE LE CODE.</h1>
+          <p>Casser les codes.</p>
+        </div>
+      </section>
+    </div>
+  )
+}
